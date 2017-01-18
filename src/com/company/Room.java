761ec44a3432eb9bf72519b9965fe5242f58123d
erikjakubowski <erik.jakubowski90@gmail.com>
2017-01-18 14:16:15 -0600

    maze

---
+++ src/com/company/Room.java
@@ -0,0 +1,17 @@
+package com.company;
+
+/**
+ * Created by erikjakubowski on 1/18/17.
+ */
+public class Room {
+    int row;
+    int col;
+    boolean wasVisited = false;
+    boolean hasBottom = true;
+    boolean hasRight = true;
+
+    public Room(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+}
